ca99972d7fd3ee2e1c7a4ecb15ee0a6cd6b75377
njokujr <njokuvictory216@gmail.com>
2023-01-24 03:44:18 +0000

    corr

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,51 +1,65 @@
 #include "sort.h"
 
 /**
-* stupify - recurrssive heapfiy function
-* @array: Array to sort
-* @heap: size of heap data
-* @i: index
-* @size: size of array
-*/
-
-void stupify(int *array, int heap, int i, int size)
+ * swap_root - A function that swap the root nodes.
+ * @array: The heap to sort.
+ * @root: The root of the heap.
+ * @hi: The higher index.
+ * @size: The size of the array.
+ * Return: Nothing
+ */
+void swap_root(int *array, size_t root, size_t hi, size_t size)
 {
-	int lar = i, left = 2 * i + 1, right = 2 * i + 2, t;
+	size_t lo = 0, mi = 0, tmp = 0;
+	int aux = 0;
 
-	if (left < heap && array[left] > array[lar])
-		lar = left;
-	if (right < heap && array[right] > array[lar])
-		lar = right;
-	if (lar != i)
+	while ((lo = (2 * root + 1)) <= hi)
 	{
-		t = array[i], array[i] = array[lar], array[lar] = t;
+		tmp = root;
+		mi = lo + 1;
+		if (array[tmp] < array[lo])
+			tmp = lo;
+		if (mi <= hi && array[tmp] < array[mi])
+			tmp = mi;
+		if (tmp == root)
+			return;
+		aux = array[root];
+		array[root] = array[tmp];
+		array[tmp] = aux;
 		print_array(array, size);
-		stupify(array, heap, lar, size);
+		root = tmp;
 	}
 }
 
 /**
-* heap_sort - Sorts array with heap sort algo
-* @array: array to sort
-* @size: Size of array to sort
-*/
-
+ * heap_sort - A function that sorts an array using heap algorithm.
+ * @array: An array to sort.
+ * @size: The size of the array.
+ * Return: Nothing.
+ */
 void heap_sort(int *array, size_t size)
 {
-	int i = size / 2 - 1, temp;
+	size_t hi = 0, gap = 0;
+	int tmp = 0;
 
-	if (array == NULL || size < 2)
-		return;
-	for (; i >= 0; i--)
-		stupify(array, size, i, size);
-	for (i = size - 1; i >= 0; i--)
+		if (array == NULL || size < 2)
+			return;
+
+		for (gap = (size - 2) / 2; 1; gap--)
+		{
+			swap_root(array, gap, size - 1, size);
+			if (gap == 0)
+				break;
+		}
+
+		hi = size - 1;
+		while (hi > 0)
 	{
-		temp = array[0];
-		array[0] = array[i];
-		array[i] = temp;
-		if (i > 0)
-			print_array(array, size);
-		stupify(array, i, 0, size);
+		tmp = array[hi];
+		array[hi] = array[0];
+		array[0] = tmp;
+		print_array(array, size);
+		hi--;
+		swap_root(array, 0, hi, size);
 	}
-
 }
