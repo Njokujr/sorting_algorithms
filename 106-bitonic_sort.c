1a37f5c786793ea1e30193839a2bec59a5c2ed47
Njokujr <111055423+Njokujr@users.noreply.github.com>
2023-01-26 13:46:21 -0800

    corr-task4

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,27 +1,91 @@
 #include "sort.h"
 
-void bitonic_sort(int *array, size_t size) {
-    if(!array) return;
-    int i, j, k;
-    for(k = 2; k <= size; k = k*2) {
-        for(j = k>>1; j > 0; j = j>>1) {
-            for(i = 0; i < size; i++) {
-                int ixj = i^j;
-                if((ixj) > i) {
-                    if((i & k) == 0 && array[i] > array[ixj]) {
-                        int temp = array[i];
-                        array[i] = array[ixj];
-                        array[ixj] = temp;
-                        printf("Swapped %d and %d\n", array[i], array[ixj]);
-                    }
-                    if((i & k) != 0 && array[i] < array[ixj]) {
-                        int temp = array[i];
-                        array[i] = array[ixj];
-                        array[ixj] = temp;
-                        printf("Swapped %d and %d\n", array[i], array[ixj]);
-                    }
-                }
-            }
-        }
-    }
+
+/**
+* swapint - swaps index's of array
+* @l: left or low index to swap
+* @r: right or high index
+*/
+
+void swapint(int *l, int *r)
+{
+	int temp;
+
+	temp = *l;
+	*l = *r;
+	*r = temp;
+}
+
+/**
+* b_merge - bitonic merge
+* @array: Array slice being merged
+* @low: lowest index
+* @count: Count of slice
+* @dir: Direction, ascending 1 descending 0
+* @size: size of total array for printing
+*/
+
+void b_merge(int *array, int low, int count, int dir, size_t size)
+{
+	int i, n;
+
+	if (count > 1)
+	{
+		n = count / 2;
+		for (i = low; i < low + n; i++)
+		{
+			if (((array[i] > array[i + n]) && dir == 1) ||
+			(dir == 0 && (array[i] < array[i + n])))
+				swapint(&array[i], &array[i + n]);
+		}
+		b_merge(array, low, n, dir, size);
+		b_merge(array, low + n, n, dir, size);
+	}
+}
+
+/**
+* b_sort - bitonic recursive sort
+* @array: array to sort
+* @low: lowest index
+* @count: Count of slice
+* @dir: Direction, ascending 1 descending 0
+* @size: size of total array for printing
+*/
+
+void b_sort(int *array, int low, int count, int dir, size_t size)
+{
+	int n;
+
+	if (count > 1)
+	{
+		n = count / 2;
+		printf("Merging [%d/%d] ", count, (int)size);
+		if (dir == 1)
+			printf("(UP):\n");
+		else
+			printf("(DOWN):\n");
+		print_array(array + low, count);
+		b_sort(array, low, n, 1, size);
+		b_sort(array, low + n, n, 0, size);
+		b_merge(array, low, count, dir, size);
+		printf("Result [%d/%d] ", count, (int)size);
+		if (dir == 1)
+			printf("(UP):\n");
+		else
+			printf("(DOWN):\n");
+		print_array(array + low, count);
+	}
+}
+
+/**
+* bitonic_sort - Sorts array using bitonic algo
+* @array: Array to sort
+* @size: Size of array
+*/
+
+void bitonic_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+	b_sort(array, 0, size, 1, size);
 }
